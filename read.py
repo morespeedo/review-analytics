fdd144079bfb62d89bd03bae0066b6d9f9457ba3
morespeedo <saad71228@gmail.com>
2023-02-16 17:14:30 +0800

    third edition

--- read.py
+++ read.py
@@ -10,5 +10,10 @@ with open('reviews.txt', 'r') as f:
 			print(len(data))
 print('檔案讀取完了，總共有', len(data), '筆資料')
 
-avg = leng/len(data)
-print('留言平均長度: ', avg)
+print('留言平均長度: ', leng/len(data))
+
+new = []
+for d in data:
+	if len(d) < 100:
+		new.append(d)
+print('一共有', len(new), '筆留言長度小於100')
