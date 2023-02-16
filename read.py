d57bc2e07e669d8afbd236456fd1802749cba812
morespeedo <saad71228@gmail.com>
2023-02-16 12:26:57 +0800

    second edition

--- read.py
+++ read.py
@@ -1,9 +1,14 @@
 data = []
-cout = 0
+leng = 0
+count = 0
 with open('reviews.txt', 'r') as f:
 	for line in f:
 		data.append(line)
+		leng += len(data[count])
 		count += 1
 		if count % 1000 == 0:
 			print(len(data))
-print(len(data))
+print('檔案讀取完了，總共有', len(data), '筆資料')
+
+avg = leng/len(data)
+print('留言平均長度: ', avg)
