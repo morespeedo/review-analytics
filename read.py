07b16970258e5801acafce699b4062357d2f981a
morespeedo <saad71228@gmail.com>
2023-02-20 15:59:06 +0800

    revise

--- read.py
+++ read.py
@@ -1,15 +1,38 @@
 data = []
-#leng = 0
+leng = 0
 count = 0
 with open('reviews.txt', 'r') as f:
 	for line in f:
 		data.append(line)
-#		leng += len(line)
+		leng += len(line)
 		count += 1
 		if count % 1000 == 0:
 			print(len(data))
 print('檔案讀取完了，總共有', len(data), '筆資料')
 
+print('留言平均長度: ', leng/len(data))
+
+new = []
+for d in data:
+	if len(d) < 100:
+		new.append(d)
+print('一共有', len(new), '筆留言長度小於100')
+
+good = []
+for d in data:
+	if 'good' in d:
+		good.append(d)
+print('一共有', len(good), '筆資料提到good')
+
+good = [d for d in data if 'good' in d]
+
+print('一共有', len(good), '筆資料提到good')
+
+bad = ['bad' in d for d in data]
+print(bad)
+
+
+#文字計數
 wc = {}
 for d in data:
 	words = d.split()
@@ -33,30 +56,3 @@ while True:
 	else:
 		print('這個字沒有出現過喔！')
 print('感謝使用本查詢功能')
-
-
-
-
-
-
-
-#print('留言平均長度: ', leng/len(data))
-
-#new = []
-#for d in data:
-#	if len(d) < 100:
-#		new.append(d)
-#print('一共有', len(new), '筆留言長度小於100')
-
-#good = []
-#for d in data:
-#	if 'good' in d:
-#		good.append(d)
-#print('一共有', len(good), '筆資料提到good')
-
-#good = [d for d in data if 'good' in d]
-
-#print('一共有', len(good), '筆資料提到good')
-
-#bad = ['bad' in d for d in data]
-#print(bad)
